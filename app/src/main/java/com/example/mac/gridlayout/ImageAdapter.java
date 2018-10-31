db563046140000caeb24368e115da22b4983470b
Mac <mac@Anonymous.local>
2018-10-31 20:53:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mac/gridlayout/ImageAdapter.java
@@ -0,0 +1,53 @@
+package com.example.mac.gridlayout;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    // Keep all Images in array
+    public Integer[] mThumbIds = {
+            R.drawable.enzo, R.drawable.laferra,
+            R.drawable.agera, R.drawable.regera,
+            R.drawable.aventador, R.drawable.camaro,
+            R.drawable.dodgevip, R.drawable.merceamg,
+            R.drawable.veneno, R.drawable.divo,
+            R.drawable.chiron, R.drawable.centenario
+
+    };
+
+    // Constructor
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+        return imageView;
+    }
+
+}
